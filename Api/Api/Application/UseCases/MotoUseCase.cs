288fbd3f0435e32b3ccefe6ec00d04761c50e386
EduardoEinsfeldt <eduardoa.einsfeldt@gmail.com>
2025-05-18 21:08:39 -0300

    UseCase

---
+++ Api/Api/Application/UseCases/MotoUseCase.cs
@@ -0,0 +1,78 @@
+﻿using Api.Application.DTO.Request;
+using Api.Application.DTO.Response;
+using Api.Domain.Entity;
+using Api.Domain.Enum;
+using Api.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Application.UseCases
+{
+    public class MotoUseCase
+    {
+        private readonly MotosContext _context;
+
+        public MotoUseCase(MotosContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CreatedMotoResponse>> GetAllMotosAsync()
+        {
+            var motos = await _context.Motos.Include(m => m.User).ToListAsync();
+
+            return motos.Select(m => new CreatedMotoResponse
+            {
+                IDMoto = m.IDMoto,
+                AnoDelançamento = DateOnly.FromDateTime(m.AnoDelançamento),
+                Quilometragem = m.Quilometragem,
+                AnoDeFabricacao = m.AnoDeFabricacao,
+                Placa = m.Placa,
+                TagDaMoto = m.TagDaMoto,
+                Chassi = m.Chassi,
+                Observacao = m.Observacao,
+                FotoDaMoto = m.FotoDaMoto,
+                IPVA = m.IPVA,
+                Licenciamento = m.Licenciamento,
+                DPVAT = m.DPVAT,
+                Combustivel = m.Combustivel.ToString(),
+                TypeMotos = m.TypeMoto.ToString(),
+                PatioAtual = m.PatioAtual,
+                PlanoAssociado = m.PlanoAssociado,
+                Multas = m.Multas,
+                HistoricoDeReparos = m.HistoricoDeReparos,
+                HistoricoDeChecks = m.HistoricoDeChecks,
+                UserId = m.User.idUser
+            }).ToList();
+        }
+
+        public async Task<Moto> CreateAsync(CreateMotoRequest request, User user)
+        {
+            var moto = new Moto
+            {
+                AnoDelançamento = request.AnoDelançamento.ToDateTime(TimeOnly.MinValue),
+                Quilometragem = request.Quilometragem,
+                AnoDeFabricacao = request.AnoDeFabricacao,
+                Placa = request.Placa,
+                TagDaMoto = request.TagDaMoto,
+                Chassi = request.Chassi,
+                Observacao = request.Observacao,
+                FotoDaMoto = request.FotoDaMoto,
+                IPVA = request.IPVA,
+                Licenciamento = request.Licenciamento,
+                DPVAT = request.DPVAT,
+                Combustivel = (TypeCombustivel)request.Combustivel,
+                TypeMoto = (TypeMoto)request.TypeMotos,
+                PatioAtual = request.PatioAtual,
+                PlanoAssociado = request.PlanoAssociado,
+                Multas = request.Multas,
+                HistoricoDeReparos = request.HistoricoDeReparos,
+                HistoricoDeChecks = request.HistoricoDeChecks,
+                User = user
+            };
+
+            _context.Motos.Add(moto);
+            await _context.SaveChangesAsync();
+            return moto;
+        }
+    }
+}
--- Api/Api/Application/UseCases/UserUseCase.cs
+++ Api/Api/Application/UseCases/UserUseCase.cs
@@ -1,6 +1,89 @@
-namespace Api.Application.UseCases;
+using Api.Application.DTO.Request;
+using Api.Application.DTO.Response;
+using Api.Domain.Entity;
+using Api.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
-public class UserUseCase
+namespace Api.Application.UseCases
 {
-    
+    public class UserUseCase
+    {
+        private readonly UserContext _context;
+
+        public UserUseCase(UserContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CreatedUserResponse>> GetAllUsersAsync()
+        {
+            var users = await _context.Users.Include(u => u.motos).ToListAsync();
+
+            return users.Select(u => new CreatedUserResponse
+            {
+                IDUser = u.idUser,
+                Nome = u.nome,
+                Email = u.email,
+                CPF = u.cpf,
+                RG = u.rg,
+                DtaNasc = DateOnly.FromDateTime(u.dtaNasc),
+                NumeroDeCadastro = u.numeroDeCadastro,
+                Ativo = u.ativo,
+                Nacionalidade = u.nacionalidade,
+                Carteira = u.carteira,
+                Enderco = u.enderco,
+                Contato = u.contato,
+                Plano = u.plano,
+                MotoId = u.motos?.IDMoto ?? 0
+            }).ToList();
+        }
+
+        public async Task<CreatedUserResponse?> GetByIdAsync(long id)
+        {
+            var u = await _context.Users.Include(u => u.motos).FirstOrDefaultAsync(u => u.idUser == id);
+            if (u == null) return null;
+
+            return new CreatedUserResponse
+            {
+                IDUser = u.idUser,
+                Nome = u.nome,
+                Email = u.email,
+                CPF = u.cpf,
+                RG = u.rg,
+                DtaNasc = DateOnly.FromDateTime(u.dtaNasc),
+                NumeroDeCadastro = u.numeroDeCadastro,
+                Ativo = u.ativo,
+                Nacionalidade = u.nacionalidade,
+                Carteira = u.carteira,
+                Enderco = u.enderco,
+                Contato = u.contato,
+                Plano = u.plano,
+                MotoId = u.motos?.IDMoto ?? 0
+            };
+        }
+
+        public async Task<User> CreateAsync(CreateUserRequest request, Moto moto)
+        {
+            var user = new User
+            {
+                nome = request.Nome,
+                email = request.Email,
+                cpf = request.CPF,
+                rg = request.RG,
+                dtaNasc = request.DtaNasc.ToDateTime(TimeOnly.MinValue),
+                numeroDeCadastro = request.NumeroDeCadastro,
+                ativo = request.Ativo,
+                nacionalidade = request.Nacionalidade,
+                carteira = request.Carteira,
+                enderco = request.Enderco,
+                contato = request.Contato,
+                plano = request.Plano,
+                motos = moto
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+    }
 }
